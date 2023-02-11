4ae9c68d1bf8469cbe5fc11f4290abd728f611b8
ZacharyYongBB <zacharyyong_1997@outlook.com>
2023-02-12 01:39:41 +0800

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Zachary on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
