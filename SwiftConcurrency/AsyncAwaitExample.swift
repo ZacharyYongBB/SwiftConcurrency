dc8cebb7f218f48272681bdff77e7b13caf9134b
ZacharyYongBB <zacharyyong_1997@outlook.com>
2023-02-15 00:58:12 +0800

    AsyncAwait

---
+++ SwiftConcurrency/AsyncAwaitExample.swift
@@ -0,0 +1,91 @@
+//
+//  AsyncAwaitExample.swift
+//  SwiftConcurrency
+//
+//  Created by Zachary on 15/2/23.
+//
+
+import SwiftUI
+
+class AsyncAwaitExampleViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func addTitle1() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.dataArray.append("Title1 : \(Thread.current)")
+        }
+    }
+    
+    func addTitle2() {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            let title = "Title2 : \(Thread.current)"
+            DispatchQueue.main.async {
+                self.dataArray.append(title)
+                
+                let title3 = "Title3 : \(Thread.current)"
+                self.dataArray.append(title3)
+            }
+        }
+    }
+    
+    func addAuthor1() async {
+        let author1 = "Author1 : \(Thread())"
+        self.dataArray.append(author1)
+        
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        
+        let author2 = "Author2 : \(Thread())"
+        await MainActor.run(body: {
+            self.dataArray.append(author2)
+            
+            let author3 = "Author3 : \(Thread.current)"
+            self.dataArray.append(author3)
+        })
+    }
+    
+    func addSomething() async {
+        
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        let something1 = "Something1 : \(Thread())"
+        await MainActor.run(body: {
+            self.dataArray.append(something1)
+            
+            let something2 = "Something2 : \(Thread())"
+            self.dataArray.append(something2)
+        })
+        
+    }
+    
+    
+}
+
+struct AsyncAwaitExample: View {
+    
+    @StateObject private var viewModel = AsyncAwaitExampleViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.dataArray, id: \.self) { data in
+                Text(data)
+            }
+        }
+        .onAppear {
+            Task {
+                await viewModel.addAuthor1()
+                await viewModel.addSomething()
+                
+                let finalText = "FINAL TEXT : \(Thread())"
+                viewModel.dataArray.append(finalText)
+            }
+//            viewModel.addTitle1()
+//            viewModel.addTitle2()
+        }
+    }
+}
+
+struct AsyncAwaitExample_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncAwaitExample()
+    }
+}
